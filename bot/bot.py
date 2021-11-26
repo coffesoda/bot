e342b8e64a394a483d6558d1705285993cf47225
coffesoda <93760980+coffesoda@users.noreply.github.com>
2021-11-26 15:50:37 +0800

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
+with open('files.json','r',encoding='utf8')as jfile:
+    jdata=json.load(jfile)
+
 client=commands.Bot(command_prefix='?')
 
 @client.event
@@ -10,8 +16,16 @@ async def on_ready():
 @client.command()
 async def i(ctx):
     await ctx.send("gggg")
+@client.command()
+async def hi(ctx):
+    await ctx.send("hi")
+
+@client.command()
+async def picture(ctx):
+    ran_pic=random.choice(jdata['ran_pic'])
+    await ctx.send(ran_pic)
 
 
-client.run("OTA2MDY3NjA2MzEyNzE4Mzk2.YYTPNw.iGShD3dbum8cbXcPYMpkJ-1SXN4")
+client.run("OTA2MDY3NjA2MzEyNzE4Mzk2.YYTPNw.dbfJw_cBb7Wggllu8EQddjxQiy8")
 
 
